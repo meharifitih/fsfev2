ecc969642d8de779776942efb5bb43c27ba693d7
meharifitih <meharifitih@gmail.com>
2022-05-20 11:15:11 +0000

    modifying app.js

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res)=>{
-        res.send('Hello Mehari!');
-
+	res.send('Hello, Mehari!');
 });
 
+app.get('/demo', (req, res)=>{
+	res.set('X-full-stack', '4-life');
+	res.status(418);
+	res.send('I prefer coffee');
+});
+
+
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`));
+
